9f412d5668398319e0864fc367f09b080d7e596c
Saad Guessous <saad.guessous@gofore.com>
2019-10-09 14:03:25 +0300

    Impl map based services

---
+++ pet-clinic-data/src/main/java/com/sguessou/sgpetclinicdata/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package com.sguessou.sgpetclinicdata.services.map;
+
+import com.sguessou.sgpetclinicdata.model.Pet;
+import com.sguessou.sgpetclinicdata.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
